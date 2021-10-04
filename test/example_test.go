a0ae4961490ab8edb13c3e4b6812d08a32c545d2
Travis Saucier <travis.saucier@sourcefuse.com>
2021-10-04 14:19:32 -0400

    update to check secret exists

--- test/example_test.go
+++ test/example_test.go
@@ -10,6 +10,7 @@ import (
 )
 
 func TestTerraformExample(t *testing.T) {
+
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../example/.",
 	}
@@ -17,6 +18,7 @@ func TestTerraformExample(t *testing.T) {
 
   // example test values
 	namespaceName := fmt.Sprintf("terratest")
+	secretName := fmt.Sprintf("nginx-secret")
 	serviceName := fmt.Sprintf("nginx")
 	servicePort := fmt.Sprintf("80")
 	k8sHost := fmt.Sprintf("nginx.terratest.svc.cluster.local")
@@ -26,30 +28,34 @@ func TestTerraformExample(t *testing.T) {
 	assert := assert.New(t)
 
   // check terraform output is valid
-  kubernetesServiceNameOutputValue := terraform.Output(t, terraformOptions, "kubernetes_service_name")
-  assert.NotNil(kubernetesServiceNameOutputValue)
-  assert.Equal(serviceName, kubernetesServiceNameOutputValue)
+	kubernetesServiceNameOutputValue := terraform.Output(t, terraformOptions, "kubernetes_service_name")
+	assert.NotNil(kubernetesServiceNameOutputValue)
+	assert.Equal(serviceName, kubernetesServiceNameOutputValue)
 
-  kubernetesServicePortOutputValue := terraform.Output(t, terraformOptions, "kubernetes_service_port")
-  assert.NotNil(kubernetesServicePortOutputValue)
-  assert.Equal(servicePort, kubernetesServicePortOutputValue)
+	kubernetesServicePortOutputValue := terraform.Output(t, terraformOptions, "kubernetes_service_port")
+	assert.NotNil(kubernetesServicePortOutputValue)
+	assert.Equal(servicePort, kubernetesServicePortOutputValue)
 
-  kubernetesHostOutputValue := terraform.Output(t, terraformOptions, "kubernetes_host")
-  assert.NotNil(kubernetesHostOutputValue)
-  assert.Equal(k8sHost, kubernetesHostOutputValue)
+	kubernetesHostOutputValue := terraform.Output(t, terraformOptions, "kubernetes_host")
+	assert.NotNil(kubernetesHostOutputValue)
+	assert.Equal(k8sHost, kubernetesHostOutputValue)
 
-  kubernetesNamespaceOutputValue := terraform.Output(t, terraformOptions, "kubernetes_namespace")
-  assert.NotNil(kubernetesNamespaceOutputValue)
-  assert.Equal(namespaceName, kubernetesNamespaceOutputValue)
+	kubernetesNamespaceOutputValue := terraform.Output(t, terraformOptions, "kubernetes_namespace")
+	assert.NotNil(kubernetesNamespaceOutputValue)
+	assert.Equal(namespaceName, kubernetesNamespaceOutputValue)
 
-  // check kubernetes configuration matches what is configured
+	// check kubernetes configuration matches what is configured
 	k8sOptions := k8s.NewKubectlOptions("", "", namespaceName)
 
-  namespace := k8s.GetNamespace(t, k8sOptions, namespaceName)
-  assert.NotNil(namespace)
+	namespace := k8s.GetNamespace(t, k8sOptions, namespaceName)
+	assert.NotNil(namespace)
 	assert.Equal(namespaceName, namespace.Name)
 
 	service := k8s.GetService(t, k8sOptions, serviceName)
-  assert.NotNil(service)
+	assert.NotNil(service)
 	assert.Equal(serviceName, service.Name)
+
+	secret := k8s.GetSecret(t, k8sOptions, secretName)
+	assert.NotNil(secret)
+	assert.Equal(secretName, secret.Name)
 }
